e7c1831dcb9c39022180c03f2c3cb63a036c631b
Matt Walter <matt.walter.tx@outlook.com>
2023-01-12 17:02:39 -0600

    Rebuild files from merge issue (again)

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
+import PropTypes from 'prop-types';
+import React, { Component, useRef } from 'react'
+import Form from './Form/Form'
+
+const Home = ({ cards }) => {
+  return (
+    <div>
+      <h2>Filter Form</h2>
+      <h6>Result Grid</h6>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+import Anton from './fonts/Anton/Anton-Regular.ttf'
+import MAGIC from './fonts/magicthegathering/MAGIC.TTF'
+
+
+const theme = createTheme({
+  typography: {
+    fontFamily: {MAGIC},
+  },
+})
+
+export default theme
