c21e887f7299f60945efef7cdea6059e215bfe43
Matt Walter <matt.walter.tx@outlook.com>
2023-01-12 16:45:21 -0600

    Begin setting Theme attributes

--- src/theme.js
+++ src/theme.js
@@ -1,27 +1,12 @@
 import { createTheme } from "@mui/material";
-// import Anton from './fonts/Anton/Anton-Regular.ttf'
+import Anton from './fonts/Anton/Anton-Regular.ttf'
+import MAGIC from './fonts/magicthegathering/MAGIC.TTF'
+
 
 const theme = createTheme({
   typography: {
-    // fontFamily: {Anton},
+    fontFamily: {MAGIC},
   },
-
-  palette: {
-
-    primary: {
-      main: '#FFCB5F',
-      light: '#FE7CE5',
-      dark: '#9966cc',
-      contrastText: '#000000'
-    },
-
-    secondary: {
-      main: '#336666',
-      light: '#996666',
-      dark: '	#cc99cc',
-      contrastText: '#000000'
-    },
-  }
 
 })
 
