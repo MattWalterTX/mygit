5a2e833700a6df8bf4e9a3104d4576ad830e6f79
Matt Walter <matt.walter.tx@outlook.com>
2023-01-11 18:11:05 -0600

    Build home page skeleton

---
+++ src/Components/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import About from '../About/About';
+import Home from '../Home/Home'
+import Header from '../Header/Header';
+import Collection from '../Collection/Collection';
+import BadURL from '../BadURL/BadURL'
+import theme from '../../theme';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import './App.css';
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="#FFCB5F" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://github.com/thomedpete/Dokes">
+        Matt Walter
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+class  App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cards: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.magicthegathering.io/v1/cards')
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+  render() {
+    return (
+      // can remove cssbaseline after theme creation
+      <div className="App">
+        <ThemeProvider>
+        <CssBaseline>
+          <Header/>
+          <Routes>
+            <Route path='/'/>{/* home */}
+            <Route path="Collection"/>
+            <Route path="/:id"/>
+            <Route path="/:id"/>
+            <Route path="/*"/>
+          </Routes>
+
+          <Box sx={{ bgcolor: '#000', p: 2, color: '#FFCB5F' }} component="footer">
+          <Copyright />
+            <Typography
+              variant="subtitle1"
+              align="center"
+              color="#FFCB5F"
+              component="p"
+            >
+              {/* Dad Joke Generator by T.I.M. */}
+            </Typography>
+          </Box>
+
+
+        </CssBaseline>
+        </ThemeProvider>
+      </div>
+    )
+  }
+}
+
+export default App;
