3990ecad8a5b1272f922d5ea9e0477ae2a92e53f
Matt Walter <matt.walter.tx@outlook.com>
2023-01-13 19:09:30 -0600

    Correct fetch call

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,6 +12,9 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import './App.css';
+import Button from '@mui/material/Button';
+import { BedroomParentSharp, ControlCameraSharp } from '@mui/icons-material';
+import { wait } from '@testing-library/user-event/dist/utils';
 
 const Copyright = () => {
   return (
@@ -26,38 +29,75 @@ const Copyright = () => {
   );
 }
 
-class  App extends Component {
+let whites = [];
+let blacks = [];
+let blues = [];
+let reds = [];
+let greens = [];
+// let allFetched = [...whites, ...blacks, ...blues, ...reds, ...greens]
+let allFetched = []
+
+class App extends Component {
   constructor() {
     super()
     this.state = {
-      cards: []
+      cards: [],
+      error: null,
+      selected: null
     }
   }
-
+  
   componentDidMount() {
-    fetch('https://api.magicthegathering.io/v1/cards')
+    fetch('https://api.magicthegathering.io/v1/cards?set=BRO&colors=W')
     .then(response => response.json())
-    .then(data => console.log(data))
-}
+    .then(data => whites.push(data.cards))
+    .then(console.log('AF', allFetched, 'W', whites))
+    // .then(data => this.setState({ cards: data.cards }))
+    .catch((error) => {
+      this.setState({ error: 'The server is experiencing some issues please try  again later'})
+    })
 
-  // componentDidMount() {
-  //    let setBRO = []
-  //   fetch('https://api.magicthegathering.io/v1/cards?page=109')
-  //   .then(response => response.json())
-  //   .then(data => console.log('109', data))
-  //   fetch('https://api.magicthegathering.io/v1/cards?page=110')
-  //   .then(response => response.json())
-  //   .then(data => console.log('110', data))
-  //   fetch('https://api.magicthegathering.io/v1/cards?page=111')
-  //   .then(response => response.json())
-  //   .then(data => console.log('111', data))
-  //   fetch('https://api.magicthegathering.io/v1/cards?page=112')
-  //   .then(response => response.json())
-  //   .then(data => console.log('112', data))
-  //   fetch('https://api.magicthegathering.io/v1/cards?page=113')
-  //   .then(response => response.json())
-  //   .then(data => console.log('113', data))
-  // }
+    fetch('https://api.magicthegathering.io/v1/cards?set=BRO&colors=B')
+    .then(response => response.json())
+    .then(data => blacks.push(data.cards))
+    // .then(data => allFetched.push(data.cards))
+
+
+    // .then(data =>  this.setState({ cards: [...this.state.cards, data.cards ] }))
+    .catch((error) => {
+      this.setState({ error: 'The server is experiencing some issues please try  again later'})
+    })
+
+
+    fetch('https://api.magicthegathering.io/v1/cards?set=BRO&colors=U')
+    .then(response => response.json())
+    .then(data => blues.push(data.cards))
+    .catch((error) => {
+      this.setState({ error: 'The server is experiencing some issues please try  again later'})
+    })
+
+
+    fetch('https://api.magicthegathering.io/v1/cards?set=BRO&colors=R')
+    .then(response => response.json())
+    .then(data => reds.push(data.cards))
+    .catch((error) => {
+      this.setState({ error: 'The server is experiencing some issues please try  again later'})
+    })
+
+
+    fetch('https://api.magicthegathering.io/v1/cards?set=BRO&colors=G')
+    .then(response => response.json())
+    .then(data => greens.push(data.cards))
+    .then(data => console.log('AF', allFetched))
+    .catch((error) => {
+      this.setState({ error: 'The server is experiencing some issues please try  again later'})
+    })
+    .then(data => console.log(whites, 'w', blues, 'u', blacks, 'b', reds, 'r', greens, 'g')) // remove log
+    .then(data => allFetched = [...whites[0], ...blacks[0], ...blues[0], ...reds[0], ...greens[0]])
+    // whites.cards.forEach(card => allFetched.push(card))
+    .then(data => this.setState({ cards: allFetched }))
+  } 
+// let allFetched = [...whites, ...blacks, ...blues, ...reds, ...greens]
 
   render() {
     return (
@@ -72,7 +112,6 @@ class  App extends Component {
             <Route path='/about' element={(<About />)} />
             <Route path='/*' element={(<BadURL />)} />
           </Routes>
-
           <Box sx={{ bgcolor: '#000', p: 2, color: '#FFCB5F' }} component="footer">
           <Copyright />
             <Typography
